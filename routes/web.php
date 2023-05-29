f01bc1f52d6740d2cd45e5f23e7a76baa098d3ca
Hasan Baig <hasan4it@gmail.com>
2023-05-30 01:40:15 +0500

    working on laravel blog

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/my-blog-posts', [BlogController::class, 'index'])->name('blog.index');
+    Route::get('/create-blog', [BlogController::class, 'create'])->name('blog.create');
+    Route::post('/store-blog', [BlogController::class, 'store'])->name('blog.store');
+    Route::get('/edit-blog', [BlogController::class, 'edit'])->name('blog.edit');
+    Route::post('/update-blog', [BlogController::class, 'update'])->name('blog.update');
+    Route::post('/delete-blog', [BlogController::class, 'delete'])->name('blog.delete');
+
+});
+
+require __DIR__.'/auth.php';
